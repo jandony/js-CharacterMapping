c43cfedd548d08dbe77c8887f4ac0e4c5da96857
Jeff Andony <jeff.andony@gmail.com>
2020-08-12 13:24:41 -0600

    Jira edits. Destructured characterArray and Character.js props.

--- src/App.js
+++ src/App.js
@@ -1,98 +1,13 @@
 import React, { Component } from "react";
 import Character from "./Components/Character/Character";
-import Data from "./Components/Data/Data";
+import { characterArray } from "./Data";
 import "./App.scss";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      listOfCharacters: [
-        {
-          name: "Character 1",
-          hp: 20,
-          attack: 100,
-          defense: 25,
-          speed: 500,
-        },
-        {
-          name: "Character 2",
-          hp: 10,
-          attack: 50,
-          defense: 50,
-          speed: 250,
-        },
-        {
-          name: "Character 3",
-          hp: 20,
-          attack: 100,
-          defense: 25,
-          speed: 500,
-        },
-        {
-          name: "Character 4",
-          hp: 10,
-          attack: 50,
-          defense: 50,
-          speed: 250,
-        },
-        {
-          name: "Character 5",
-          hp: 20,
-          attack: 100,
-          defense: 25,
-          speed: 500,
-        },
-        {
-          name: "Character 6",
-          hp: 10,
-          attack: 50,
-          defense: 50,
-          speed: 250,
-        },
-        {
-          name: "Character 7",
-          hp: 20,
-          attack: 100,
-          defense: 25,
-          speed: 500,
-        },
-        {
-          name: "Character 8",
-          hp: 10,
-          attack: 50,
-          defense: 50,
-          speed: 250,
-        },
-        {
-          name: "Character 9",
-          hp: 20,
-          attack: 100,
-          defense: 25,
-          speed: 500,
-        },
-        {
-          name: "Character 10",
-          hp: 10,
-          attack: 50,
-          defense: 50,
-          speed: 250,
-        },
-        {
-          name: "Character 11",
-          hp: 20,
-          attack: 100,
-          defense: 25,
-          speed: 500,
-        },
-        {
-          name: "Character 12",
-          hp: 10,
-          attack: 50,
-          defense: 50,
-          speed: 250,
-        },
-      ],
+      listOfCharacters: characterArray,
     };
   }
   render() {
@@ -101,13 +16,7 @@ class App extends Component {
         <div className="flex-container">
           <div className="flex-row">
             {this.state.listOfCharacters.map((character) => (
-              <Character
-                CharacterName={character.name}
-                CharacterHP={character.hp}
-                CharacterAttack={character.attack}
-                CharacterDefense={character.defense}
-                CharacterSpeed={character.speed}
-              />
+              <Character character={character} />
             ))}
           </div>
         </div>
--- src/Components/Character/Character.js
+++ src/Components/Character/Character.js
@@ -1,71 +1,59 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Character extends Component {
-  render() {
-    return (
-      <div className="card flex-column">
-        <h1>{this.props.CharacterName}</h1>
-        <div className="card-body">
-          <p>
-            <b>HP:</b> {this.props.CharacterHP}
-            <div className="progressBar">
-              <div
-                className="stat"
-                style={{
-                  width:
-                    this.props.CharacterHP < 100
-                      ? `${this.props.CharacterHP}%`
-                      : "100%",
-                }}
-              ></div>
-            </div>
-          </p>
-          <p>
-            <b>Attack:</b> {this.props.CharacterAttack}
-            <div className="progressBar">
-              <div
-                className="stat"
-                style={{
-                  width:
-                    this.props.CharacterAttack < 100
-                      ? `${this.props.CharacterAttack}%`
-                      : "100%",
-                }}
-              ></div>
-            </div>
-          </p>
-          <p>
-            <b>Defense:</b> {this.props.CharacterDefense}
-            <div className="progressBar">
-              <div
-                className="stat"
-                style={{
-                  width:
-                    this.props.CharacterDefense < 100
-                      ? `${this.props.CharacterDefense}%`
-                      : "100%",
-                }}
-              ></div>
-            </div>
-          </p>
-          <p>
-            <b>Speed:</b> {this.props.CharacterSpeed}
-            <div className="progressBar">
-              <div
-                className="stat"
-                style={{
-                  width:
-                    this.props.CharacterSpeed < 100
-                      ? `${this.props.CharacterSpeed}%`
-                      : "100%",
-                }}
-              ></div>
-            </div>
-          </p>
-        </div>
+function Character({ character }) {
+  const { name, hp, attack, defense, speed } = character;
+
+  return (
+    <div className="card flex-column">
+      <h1>{name}</h1>
+      <div className="card-body">
+        <p>
+          <b>HP:</b> {hp}
+          <div className="progressBar">
+            <div
+              className="stat"
+              style={{
+                width: hp < 100 ? `${hp}%` : "100%",
+              }}
+            ></div>
+          </div>
+        </p>
+        <p>
+          <b>Attack:</b> {attack}
+          <div className="progressBar">
+            <div
+              className="stat"
+              style={{
+                width: attack < 100 ? `${attack}%` : "100%",
+              }}
+            ></div>
+          </div>
+        </p>
+        <p>
+          <b>Defense:</b> {defense}
+          <div className="progressBar">
+            <div
+              className="stat"
+              style={{
+                width: defense < 100 ? `${defense}%` : "100%",
+              }}
+            ></div>
+          </div>
+        </p>
+        <p>
+          <b>Speed:</b> {speed}
+          <div className="progressBar">
+            <div
+              className="stat"
+              style={{
+                width: speed < 100 ? `${speed}%` : "100%",
+              }}
+            ></div>
+          </div>
+        </p>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Character;
